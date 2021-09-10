cc934fbba86711d26f012846835a8d3bd90ef679
Avadhoot Prakash Autade <avadhootautade111@gmail.com>
2021-09-10 14:40:58 +0530

    Add files via upload

---
+++ custmansys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for custmansys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custmansys.settings')
+
+application = get_asgi_application()
